5ef0423ddc8efc9cc0746e610f70650a9632fea7
Guru Vamsi Policharla <guruvamsi.policharla@gmail.com>
2025-04-03 14:54:57 -0700

    starting aggregate key

--- src/sps/mod.rs
+++ src/sps/mod.rs
@@ -1,10 +1,11 @@
 use ark_ec::AffineRepr;
 use ark_ec::{pairing::Pairing, PrimeGroup};
-use ark_ff::PrimeField;
+use ark_ff::{Field, PrimeField};
+use ark_poly::Polynomial;
 use ark_std::{One, UniformRand, Zero};
 
 use crate::crs::PowersOfTau;
-use crate::utils::hash_to_bytes;
+use crate::utils::{hash_to_bytes, lagrange_poly};
 
 #[derive(Clone, Debug)]
 pub struct CRS<E: Pairing> {
@@ -17,6 +18,51 @@ pub struct CRS<E: Pairing> {
     pub n: usize,
     pub powers_of_tau: PowersOfTau<E>,
     pub a_powers_of_tau: Vec<E::G1>,
+
+    // preprocessed lagrange polynomials
+    pub li: Vec<E::G1>,
+    pub li_minus0: Vec<E::G1>,
+    pub li_x: Vec<E::G1>,
+    pub li_lj_z: Vec<Vec<E::G1>>,
+
+    pub ali: Vec<E::G1>,
+    pub ali_minus0: Vec<E::G1>,
+    pub ali_x: Vec<E::G1>,
+    pub ali_lj_z: Vec<Vec<E::G1>>,
+}
+
+#[derive(Clone, Debug)]
+pub struct SK<E: Pairing> {
+    k: [[E::ScalarField; 2]; 2],
+}
+
+#[derive(Clone, Debug)]
+pub struct VK<E: Pairing> {
+    pub vk: [E::G1; 2],
+
+    pub a0_neg: E::G1,
+    pub a1_neg: E::G1,
+
+    pub g1_gen_neg: E::G1,
+
+    pub ua: [E::G1; 2],
+    pub va: [E::G1; 2],
+
+    // hints
+    pub id: usize, // position in the committee of n parties
+
+    pub ka_li: [E::G1; 2],
+    pub ka_li_minus0: [E::G1; 2],
+    pub ka_li_lj_z: Vec<[E::G1; 2]>,
+    pub ka_li_x: [E::G1; 2],
+}
+
+#[derive(Clone, Debug)]
+pub struct Sig<E: Pairing> {
+    pub s1: [E::G2; 2],
+    pub s2: [E::G2; 2],
+    pub s3: [E::G2; 2],
+    pub s4: E::G1,
 }
 
 impl<E: Pairing> CRS<E> {
@@ -67,7 +113,9 @@ impl<E: Pairing> CRS<E> {
             E::G2::generator() * (b_f[0] * v[0][1] + b_f[1] * v[1][1]),
         ];
 
+        // powers of tau
         let tau = E::ScalarField::rand(&mut rng);
+
         let powers_of_tau = PowersOfTau::<E>::setup(n, tau);
 
         let a_powers_of_tau = powers_of_tau
@@ -75,6 +123,66 @@ impl<E: Pairing> CRS<E> {
             .iter()
             .map(|&gtaui| gtaui * a[1])
             .collect::<Vec<_>>();
+
+        // lagrange powers
+        let mut li_evals: Vec<E::ScalarField> = vec![E::ScalarField::zero(); n];
+        let mut li_evals_minus0: Vec<E::ScalarField> = vec![E::ScalarField::zero(); n];
+        let mut li_evals_x: Vec<E::ScalarField> = vec![E::ScalarField::zero(); n];
+
+        let tau_inv = tau.inverse().unwrap();
+        for i in 0..n {
+            let li = lagrange_poly(n, i);
+            li_evals[i] = li.evaluate(&tau);
+
+            li_evals_minus0[i] = li_evals[i] - li.coeffs[0];
+
+            li_evals_x[i] = li_evals_minus0[i] * tau_inv;
+        }
+
+        let z_eval = tau.pow(&[n as u64]) - E::ScalarField::one();
+        let z_eval_inv = z_eval.inverse().unwrap();
+
+        let mut li = vec![E::G1::zero(); n];
+        let mut ali = vec![E::G1::zero(); n];
+        for i in 0..n {
+            li[i] = E::G1::generator() * li_evals[i];
+            ali[i] = E::G1::generator() * (li_evals[i] * a[1]);
+        }
+
+        let mut li_minus0 = vec![E::G1::zero(); n];
+        let mut ali_minus0 = vec![E::G1::zero(); n];
+        for i in 0..n {
+            li_minus0[i] = E::G1::generator() * li_evals_minus0[i];
+            ali_minus0[i] = E::G1::generator() * (li_evals_minus0[i] * a[1]);
+        }
+
+        let mut li_x = vec![E::G1::zero(); n];
+        let mut ali_x = vec![E::G1::zero(); n];
+        for i in 0..n {
+            li_x[i] = E::G1::generator() * li_evals_x[i];
+            ali_x[i] = E::G1::generator() * (li_evals_x[i] * a[1]);
+        }
+
+        let mut li_lj_z = vec![vec![E::G1::zero(); n]; n];
+        let mut ali_lj_z = vec![vec![E::G1::zero(); n]; n];
+        for i in 0..n {
+            for j in 0..n {
+                li_lj_z[i][j] = if i == j {
+                    E::G1::generator() * ((li_evals[i] * li_evals[i] - li_evals[i]) * z_eval_inv)
+                } else {
+                    E::G1::generator() * (li_evals[i] * li_evals[j] * z_eval_inv)
+                };
+
+                ali_lj_z[i][j] = if i == j {
+                    E::G1::generator()
+                        * ((li_evals[i] * li_evals[i] - li_evals[i]) * z_eval_inv * a[1])
+                } else {
+                    E::G1::generator() * (li_evals[i] * li_evals[j] * z_eval_inv * a[1])
+                };
+            }
+        }
+
+        // a_lagrange powers
 
         Self {
             ua,
@@ -85,34 +193,16 @@ impl<E: Pairing> CRS<E> {
             n,
             powers_of_tau,
             a_powers_of_tau,
+            li,
+            li_minus0,
+            li_x,
+            li_lj_z,
+            ali,
+            ali_minus0,
+            ali_x,
+            ali_lj_z,
         }
     }
-}
-
-#[derive(Clone, Debug)]
-pub struct SK<E: Pairing> {
-    k: [[E::ScalarField; 2]; 2],
-}
-
-#[derive(Clone, Debug)]
-pub struct VK<E: Pairing> {
-    pub vk: [E::G1; 2],
-
-    pub a0_neg: E::G1,
-    pub a1_neg: E::G1,
-
-    pub g1_gen_neg: E::G1,
-
-    pub ua: [E::G1; 2],
-    pub va: [E::G1; 2],
-}
-
-#[derive(Clone, Debug)]
-pub struct Sig<E: Pairing> {
-    pub s1: [E::G2; 2],
-    pub s2: [E::G2; 2],
-    pub s3: [E::G2; 2],
-    pub s4: E::G1,
 }
 
 impl<E: Pairing> SK<E> {
@@ -129,6 +219,57 @@ impl<E: Pairing> SK<E> {
             ],
         ];
         Self { k }
+    }
+
+    pub fn get_pk(&self, crs: &CRS<E>, id: usize) -> VK<E> {
+        let ka0 =
+            crs.powers_of_tau.powers_of_g[0] * self.k[0][0] + crs.a_powers_of_tau[0] * self.k[0][1];
+        let ka1 =
+            crs.powers_of_tau.powers_of_g[0] * self.k[1][0] + crs.a_powers_of_tau[0] * self.k[1][1];
+
+        let vk = [ka0, ka1];
+
+        let a0_neg = -crs.powers_of_tau.powers_of_g[0].into_group();
+        let a1_neg = -crs.a_powers_of_tau[0];
+
+        let g1_gen_neg = -E::G1::generator();
+
+        let ka_li = [
+            crs.li[id] * self.k[0][0] + crs.ali[id] * self.k[0][1],
+            crs.li[id] * self.k[1][0] + crs.ali[id] * self.k[1][1],
+        ];
+
+        let ka_li_minus0 = [
+            crs.li_minus0[id] * self.k[0][0] + crs.ali_minus0[id] * self.k[0][1],
+            crs.li_minus0[id] * self.k[1][0] + crs.ali_minus0[id] * self.k[1][1],
+        ];
+
+        let ka_li_x = [
+            crs.li_x[id] * self.k[0][0] + crs.ali_x[id] * self.k[0][1],
+            crs.li_x[id] * self.k[1][0] + crs.ali_x[id] * self.k[1][1],
+        ];
+
+        let mut ka_li_lj_z = vec![];
+        for j in 0..crs.n {
+            ka_li_lj_z.push([
+                crs.li_lj_z[id][j] * self.k[0][0] + crs.ali_lj_z[id][j] * self.k[0][1],
+                crs.li_lj_z[id][j] * self.k[1][0] + crs.ali_lj_z[id][j] * self.k[1][1],
+            ]);
+        }
+
+        VK {
+            vk,
+            a0_neg,
+            a1_neg,
+            g1_gen_neg,
+            ua: crs.ua.clone(),
+            va: crs.va.clone(),
+            id,
+            ka_li,
+            ka_li_minus0,
+            ka_li_lj_z,
+            ka_li_x,
+        }
     }
 
     pub fn sign(&self, crs: &CRS<E>, m: E::G2) -> Sig<E> {
@@ -158,27 +299,6 @@ impl<E: Pairing> SK<E> {
 }
 
 impl<E: Pairing> VK<E> {
-    pub fn new(crs: &CRS<E>, sk: &SK<E>) -> Self {
-        let vk = [
-            crs.powers_of_tau.powers_of_g[0] * sk.k[0][0] + crs.a_powers_of_tau[0] * sk.k[0][1],
-            crs.powers_of_tau.powers_of_g[0] * sk.k[1][0] + crs.a_powers_of_tau[0] * sk.k[1][1],
-        ];
-
-        let a0_neg = -crs.powers_of_tau.powers_of_g[0].into_group();
-        let a1_neg = -crs.a_powers_of_tau[0];
-
-        let g1_gen_neg = -E::G1::generator();
-
-        Self {
-            vk,
-            a0_neg,
-            a1_neg,
-            g1_gen_neg,
-            ua: crs.ua.clone(),
-            va: crs.va.clone(),
-        }
-    }
-
     pub fn verify(&self, m: &E::G2, sig: &Sig<E>) {
         // check 1
         let lhs = [
@@ -229,12 +349,12 @@ impl<E: Pairing> VK<E> {
 mod tests {
     use super::*;
     use ark_bls12_381::Bls12_381 as E;
-    use ark_bls12_381::G1Projective as G1;
+    // use ark_bls12_381::Fr as F;
     use ark_bls12_381::G2Projective as G2;
 
     #[test]
     fn test_crs() {
-        let n = 10;
+        let n = 1 << 5;
         let crs = CRS::<E>::new(n);
         assert_eq!(crs.n, n);
         assert_eq!(crs.ua.len(), 2);
@@ -248,14 +368,24 @@ mod tests {
 
     #[test]
     fn test_signing() {
-        let n = 10;
+        let n = 1 << 5;
         let crs = CRS::<E>::new(n);
         let sk = SK::<E>::new();
-        let vk = VK::new(&crs, &sk);
+        let vk = sk.get_pk(&crs, 0);
         let m = G2::rand(&mut ark_std::test_rng());
-
         let sig = sk.sign(&crs, m);
-
         vk.verify(&m, &sig);
+    }
+
+    #[test]
+    fn test_aggregation() {
+        let n = 1 << 5;
+        let crs = CRS::<E>::new(n);
+        let mut sk = Vec::new();
+        let mut vk = Vec::new();
+        for i in 0..n {
+            sk.push(SK::<E>::new());
+            vk.push(sk[i].get_pk(&crs, i));
+        }
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,7 @@
+use ark_ff::FftField;
+use ark_poly::{
+    univariate::DensePolynomial, EvaluationDomain, Evaluations, Radix2EvaluationDomain,
+};
 use ark_serialize::CanonicalSerialize;
 
 pub fn hash_to_bytes<T: CanonicalSerialize>(inp: T) -> [u8; 32] {
@@ -6,4 +10,21 @@ pub fn hash_to_bytes<T: CanonicalSerialize>(inp: T) -> [u8; 32] {
     let hash = blake3::hash(bytes.as_slice());
     let hash_bytes = hash.as_bytes();
     *hash_bytes
+}
+
+// 1 at omega^i and 0 elsewhere on domain {omega^i}_{i \in [n]}
+pub fn lagrange_poly<F: FftField>(n: usize, i: usize) -> DensePolynomial<F> {
+    debug_assert!(i < n);
+    //todo: check n is a power of 2
+    let mut evals = vec![];
+    for j in 0..n {
+        let l_of_x: u64 = if i == j { 1 } else { 0 };
+        evals.push(F::from(l_of_x));
+    }
+
+    //powers of nth root of unity
+    let domain = Radix2EvaluationDomain::<F>::new(n).unwrap();
+    let eval_form = Evaluations::from_vec_and_domain(evals, domain);
+    //interpolated polynomial over the n points
+    eval_form.interpolate()
 }
